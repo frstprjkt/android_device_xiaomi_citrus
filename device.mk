3fd281aff5b23d17e4362d5763da79576dabcd71
Vayruz Rafli <vayruzrafli79@outlook.co.id>
2023-05-21 05:58:19 +0700

    citrus: Initial bringup to sm6225-common tree
    Change-Id: I386b7f1ef905794eef5c2885e5c0374858b47bec

---
+++ device.mk
@@ -0,0 +1,70 @@
+# Copyright (C) 2023 Paranoid Android
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# AAPT
+PRODUCT_AAPT_CONFIG := normal
+PRODUCT_AAPT_PREF_CONFIG := xxhdpi
+
+# API Level
+PRODUCT_SHIPPING_API_LEVEL := 29
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2340
+TARGET_SCREEN_WIDTH := 1080
+
+# Camera
+PRODUCT_PACKAGES += \
+    libpiex_shim
+
+# Consumer IR
+PRODUCT_PACKAGES += \
+    android.hardware.ir@1.0-impl \
+    android.hardware.ir@1.0-service
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.consumerir.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.consumerir.xml
+
+# Fingerprint
+PRODUCT_VENDOR_PROPERTIES += \
+    ro.hardware.fp.sideCap=true
+
+# Kernel
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,$(LOCAL_PATH)-kernel/vendor-modules,$(TARGET_COPY_OUT_VENDOR)/lib/modules)
+
+# Keylayout
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/keylayout/,$(TARGET_COPY_OUT_VENDOR)/usr/keylayout)
+
+# Overlays
+PRODUCT_PACKAGES += \
+    AOSPACitrusFrameworksOverlay \
+    CitrusFrameworksOverlay \
+    CitrusSystemUIOverlay \
+    CitrusWifiOverlay \
+    CitrusSettingsProviderOverlay
+
+# Rootdir / Init files
+PRODUCT_PACKAGES += \
+    init.device.rc
+
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
+
+# Inherit from vendor blobs
+$(call inherit-product, vendor/xiaomi/citrus/citrus-vendor.mk)
+
+# Inherit from sm6225-common
+$(call inherit-product, device/xiaomi/sm6225-common/common.mk)
