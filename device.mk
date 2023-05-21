878a305134b5d15d634f8a81af8c38139129a509
Vayruz Rafli <vayruzrafli79@outlook.co.id>
2023-05-21 07:19:26 +0700

    citrus: Import audio_platform_info_intcodec from stock
    Change-Id: Ic390637bd5e7699e2d31f4fa0a7b47b40220f3a7

--- device.mk
+++ device.mk
@@ -19,6 +19,10 @@ PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 # API Level
 PRODUCT_SHIPPING_API_LEVEL := 29
 
+# Audio
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/audio/,$(TARGET_COPY_OUT_VENDOR)/etc)
+
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2340
 TARGET_SCREEN_WIDTH := 1080
